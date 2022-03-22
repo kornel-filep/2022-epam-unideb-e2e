a98da134ff8f4f95c569896ffac11db8586dd38c
Kornel Filep <kornel.filep@gmail.com>
2022-03-22 08:45:03 +0100

    update path for report

--- src/test/java/com/epam/ta/uni/TestRunnerIT.java
+++ src/test/java/com/epam/ta/uni/TestRunnerIT.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com.epam.ta.uni",
         stepNotifications = true,
-        plugin = {"pretty", "html:target/cucumber-reports"}
+        plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class TestRunnerIT {
 
